b4e1ea96d8a2cfdfec0a2f89ec3029209757f6a4
Lane Hamilton <aleyna.hamilton@gmail.com>
2023-04-22 15:36:14 -0700

    Database hooked up to login user input

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,35 +1,60 @@
+// note: these are middleware
+
 // temporary import: don't have info on database yet
-const db = require('../models/models');
+const db = require('../models/mixMasterModel');
 
 const bcrypt = require('bcryptjs');
 const SALT_WORK_FACTOR = 10;
 
 const userController = {};
 
+// create user in database
 userController.createUser = (req, res, next) => {
+  console.log('controller entered');
   // get username and password from form input submission
-  const { username, password } = req.body;
+  const { username, password, firstname } = req.body[0];
+  console.log('body: ', req.body);
   // if username or password fields not filled in, return error to express global error handler
   if (!username || !password) {
     return next({
-      'Error occurred in userController.createUser: must include username AND password',
-
-    })
+      log: 'Error occurred in userController.createUser',
+      message: 'Error',
+    });
   }
   // check if username already taken
-  const query = `SELECT * FROM user
-    WHERE username = ${username}`;
+  const query = `SELECT * FROM users
+    WHERE username = '${username}'`;
 
-  db.query(query)
-    .then(data => {
-      // if username exists, return error to express global error handler
-      if (data) {
-        return next({
-          log: 'Error occurred in userController.createUser: ' + JSON.stringify(new Error('username already exists in database')),
-          message: 'Error: username already exists'
-        })
-      }
-      // if username doesn't exist, bcrypt password and insert account into user table in SQL database
-    })
+  db.query(query).then((data) => {
+    // if username exists, return error to express global error handler
+    if (!data) {
+      return next({
+        log:
+          'Error occurred in userController.createUser: ' +
+          JSON.stringify(new Error('username already exists in database')),
+        message: 'Error: username already exists',
+      });
+    }
+    // if username doesn't exist, bcrypt password and insert account into user table in SQL database
+    console.log('reached hash');
+    bcrypt.hash(password, SALT_WORK_FACTOR).then((hash) => {
+      console.log(hash);
+      const values = [username, hash, firstname];
+      const query = `INSERT INTO users (username, password, firstname)
+            VALUES ($1, $2, $3)`;
 
+      db.query(query, values).then((data) => {
+        console.log('added to database');
+        res.locals.result = data.rows[0];
+        next();
+      });
+    });
+  });
 };
+
+// verify user info upon login
+// userController.verifyUser = (req, res, next) => {
+
+// };
+
+module.exports = userController;
---
+++ server/routers/loginRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const userController = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/', userController.createUser, (req, res) =>
+  res.status(200).json(res.locals.result)
+);
+
+module.exports = router;
