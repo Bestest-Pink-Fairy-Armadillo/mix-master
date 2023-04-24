3e73955de19e543699c624fee857c15797f72a89
domc13 <119551389+domc13@users.noreply.github.com>
2023-04-24 10:37:53 -0700

    DomJustin/additional save

--- src/containers/RecipeContainer.jsx
+++ src/containers/RecipeContainer.jsx
@@ -13,26 +13,25 @@ function RecipeContainer(props) {
     }
 
 
-    useEffect(() => {
-      
-      const fetch1 = fetch(`https://api.api-ninjas.com/v1/cocktail?name=${props.search}`, options)
-      .then(response => response.json());
-      const fetch2 = fetch(`https://api.api-ninjas.com/v1/cocktail?ingredients=${props.search}`, options)
-      .then(response => response.json());
-
-      Promise.all([fetch1, fetch2])
-      .then(datas => { 
-        if(Object.keys(datas[0])[0] && Object.keys(datas[1])[0] !== 'error'){
-        const combinedData = datas.flat();
-        console.log(combinedData);
-        setData(combinedData);
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      });
+  useEffect(() => {
     
-      }, [props.search]);
+    const fetch1 = fetch(`https://api.api-ninjas.com/v1/cocktail?name=${props.search}`, options)
+    .then(response => response.json());
+    const fetch2 = fetch(`https://api.api-ninjas.com/v1/cocktail?ingredients=${props.search}`, options)
+    .then(response => response.json());
+
+    Promise.all([fetch1, fetch2])
+    .then(datas => { 
+      if(Object.keys(datas[0])[0] && Object.keys(datas[1])[0] !== 'error'){
+      const combinedData = datas.flat();
+      console.log(combinedData);
+      setData(combinedData);
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    }); 
+  }, [props.search]);
 
     
 
